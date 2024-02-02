b95ca8dbb34dd27864fb627693cd2174fd71ea7f
minimalparts <aurelie.herbelot@cantab.net>
2024-02-02 16:38:03 +0100

    Moved authentification to its own module

---
+++ app/auth/controllers.py
@@ -0,0 +1,91 @@
+# SPDX-FileCopyrightText: 2022 PeARS Project, <community@pearsproject.org>, 
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+# Import flask dependencies
+from flask import Blueprint, request, render_template, send_from_directory, make_response
+from flask import current_app
+from flask_cors import cross_origin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+# Import the database object from the main app module
+from app import app
+
+# Import matrix manipulation modules
+import numpy as np
+from scipy import sparse
+
+# Import utilities
+import re
+import requests
+from os.path import dirname, join, realpath, isfile
+from flask import jsonify, Response
+from app import LOCAL_RUN
+
+# Define the blueprint:
+auth = Blueprint('auth', __name__, url_prefix='/auth')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    password = PasswordField('Password')
+    submit = SubmitField('Submit')
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    # Declare the login form using FlaskForm library
+    form = LoginForm(request.form)
+    print(form)
+    # Flask message injected into the page, in case of any errors
+    msg = None
+    # check if both http method is POST and form is valid on submit
+    if form.validate_on_submit():
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str)
+        # send authorization message to on my disk
+        if LOCAL_RUN:
+            url = 'http://localhost:9191/api' #Local test
+        else:
+            url = ' https://demo.onmydisk.net/signin/'
+        data = {'action': 'signin', 'username': username, 'password': password}
+        user_info = requests.post(url, json=data) 
+        if user_info == None:
+            msg = "Incorrect credentials"
+            return render_template( 'auth/login.html', form=form, msg=msg)
+        else:
+            access_token = user_info.cookies.get('OMD_SESSION_ID')
+            print(user_info.json())
+            print(user_info.cookies)
+            username = user_info.json()['username']
+            # Create a new response object
+            resp_frontend = make_response(render_template( 'search/user.html', welcome="Welcome "+username))
+            # Transfer the cookies from backend response to frontend response
+            for name, value in user_info.cookies.items():
+                print("SETTING COOKIE:",name,value)
+                resp_frontend.set_cookie(name, value, samesite='Lax')
+            return resp_frontend
+            #return render_template('search/user.html', welcome="Welcome "+username)
+    else:
+       msg = "Unknown user"
+       return render_template( 'auth/login.html', form=form, msg=msg)
+
+
+@auth.route('/logout', methods=['GET','POST'])
+def logout():
+    access_token = request.cookies.get('OMD_SESSION_ID')
+    if LOCAL_RUN:
+        url = 'http://localhost:9191/api' #Local test
+    else:
+        url = ' https://demo.onmydisk.net/signout/'
+    data = {'action': 'signout', 'session_id': access_token}
+    logout_confirmation = requests.post(url, json=data, headers={'accept':'application/json', 'Authorization': 'token:'+access_token})
+    if logout_confirmation.status_code == requests.codes.ok:
+        print("Logging out")
+    else:
+        print("Logged out")
+    # Create a new response object
+    resp_frontend = make_response(render_template( 'search/anonymous.html'))
+    resp_frontend.set_cookie('OMD_SESSION_ID', '', expires=0, samesite='Lax')
+    return resp_frontend
