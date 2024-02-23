265bc6745dd61aedea9fa8de33034f7d66af409e
minimalparts <aurelie.herbelot@cantab.net>
2024-02-23 21:15:23 +0100

    Fixed half of moving logic

--- app/api/controllers.py
+++ app/api/controllers.py
@@ -67,15 +67,19 @@ def return_url_delete(path):
 
 @api.route('/urls/move', methods=["GET","POST"])
 @login_required
-def return_rename():
-    """ TODO: fix npz and posix when moving from personal to shared
-    """
+def api_move():
     src = request.args.get('src')
     target = request.args.get('target')
     username = session['username']
+    success, message = return_move(src, target, username)
+    print(success, message)
+    return render_template('search/user.html', welcome = message)
 
-    print("SRC",src)
-    print("TARGET",target)
+
+def return_move(src, target, username):
+    """ TODO: fix npz and posix when moving from personal to shared
+    """
+    message = ""
     
     if LOCAL_RUN:
         path_personal = 'http://localhost:9090/static/testdocs/'+username
@@ -88,8 +92,8 @@ def return_rename():
     u = db.session.query(Urls).filter_by(url=src).first()
     if u is None:
         print(">>ERROR The source url was not found in the database.")
-        flash("The source url was not found in the database.")
-        return False
+        message = "The source url was not found in the database."
+        return False, message
 
     #Check source url belongs to user
     pod_name = u.pod
@@ -98,38 +102,36 @@ def return_rename():
         assert pod_username == username
     except AssertionError as err:
         print(">>ERROR You cannot move other users' documents.")
-        flash("You cannot move other users' documents.")
-        return False
+        message = "You cannot move other users' documents."
+        return False, message
 
     #Check target url belongs to user or is shared
     try:
         assert path_personal in target or path_shared in target
     except AssertionError as err:
         print(">>ERROR You cannot move files to another user's directory.")
-        flash("You cannot move other users' documents.")
-        return False
+        message = "You cannot move other users' documents."
+        return False, message
   
     #Check case where target is a directory
     src_name = basename(src)
     if target[-1] == '/':
         target = join(target,src_name)
-
-    #If url has moved between shared and non shared locations,
-    #we must change the npz and pos files
-    if (src in path_personal and target in path_shared) or \
-            (src in path_shared and target in path_personal):
-        move_npz_pos(src, target)
-    else:
-        rename_idx_to_url(username, src, target)
-
-    #Rename in DB
     target_name = basename(target)
-    print(target_name)
-    u.url = target
-    if u.title == src_name:
-        u.title = target_name
 
-    db.session.add(u)
-    db.session.commit()
-    print("Moved file "+src+" to "+target)
-    return True
+    #The simple case: the file name has not changed and there is
+    #no moving between private and shared pods
+    if src_name == target_name:
+        print("Names are the same.")
+        print(src,target,path_personal)
+        if (path_personal in src and path_personal in target) or \
+            (path_shared in src and path_shared in target):
+            print("No moving across pods.")
+            rename_idx_to_url(username, src, target)
+            u.url = target
+            db.session.add(u)
+            db.session.commit()
+            message = "Moved file "+src+" to "+target+'.'
+            return True, message
+    message = "This is a complicated case. Ignoring it for now."
+    return False, message
