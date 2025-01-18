dd5ef2027542f52e7ab2084a8100a6b2d502a97c
minimalparts <aurelie.herbelot@cantab.net>
2025-01-18 12:02:41 +0100

    Fixes to CLI and website indexing

--- app/subscriptions/controllers.py
+++ app/subscriptions/controllers.py
@@ -41,7 +41,7 @@ def show_all_sites():
     sites_in_db = db.session.query(Sites).all()
     all_sites = []
     for site in sites_in_db:
-        all_sites.append({'url': site.url, 'title': site.title, 'owner': site.owner, 'description': site.description, 'subscribed': site.subscribed})
+        all_sites.append({'url': site.url, 'name': site.name, 'title': site.title, 'owner': site.owner, 'description': site.description, 'subscribed': site.subscribed})
     print(all_sites)
     return render_template("subscriptions/allsites.html", sites=all_sites)
 
@@ -51,8 +51,10 @@ def show_all_sites():
 def pull_sites_from_gateway():
     all_sites = []
     url = join(OMD_PATH,'sites')
+    access_token = request.cookies.get('OMD_SESSION_ID')
     data = {'action': 'list'}
-    resp = requests.post(url, timeout=30, headers={'Authorization': 'token:'+AUTH_TOKEN}, json=data)
+    #resp = requests.post(url, timeout=30, headers={'Authorization': 'token:'+AUTH_TOKEN}, json=data)
+    resp = requests.post(url, json=data, headers={'accept':'application/json', 'Authorization': 'token:'+access_token})
     json = resp.json()['list']
     for site in json:
         owner = site['owner']
@@ -70,18 +72,11 @@ def pull_sites_from_gateway():
 def subscribe_to_site():
     site_name=request.args.get('sitename')
     access_token = request.cookies.get('OMD_SESSION_ID')
+    print(access_token)
     data = {'action': 'subscribe', 'site': site_name}
     resp = requests.post(OMD_PATH, json=data, headers={'accept':'application/json', 'Authorization': 'token:'+access_token})
-    s = db.session.query(Sites).filter_by(title=site_name).first()
+    s = db.session.query(Sites).filter_by(name=site_name).first()
     s.subscribed = True
-    #html_links = process_html_links(s.url+'?direct')
-    #print(s.url,html_links)
-    #for link in html_links:
-    #    title, body_str, snippet, _ = extract_html(link)
-    #    description = ""
-    #    pod_path = ""
-    #    language = "en"
-    #    run_indexing(link, pod_path, title, snippet, description, language, body_str)
     db.session.add(s)
     db.session.commit()
     flash(f"Subscribed to site {site_name}")
@@ -98,13 +93,13 @@ def unsubscribe_from_site():
     url = OMD_PATH
     data = {'action': 'unsubscribe', 'site': site_name}
     resp = requests.post(url, json=data, headers={'accept':'application/json', 'Authorization': 'token:'+access_token})
-    s = db.session.query(Sites).filter_by(title=site_name).first()
+    s = db.session.query(Sites).filter_by(name=site_name).first()
     s.subscribed = False
-    #delete_urls_recursively(s.url)
+    delete_urls_recursively(s.url)
     db.session.add(s)
     db.session.commit()
     flash(f"Unsubscribed from site {site_name}")
-    return redirect(url_for('subscriptions.index'))
+    return redirect(url_for('subscriptions.show_all_sites'))
 
 @subscriptions.route("/update_site_subscriptions/", methods=["POST"])
 @login_required
@@ -120,8 +115,9 @@ def update_site_subscriptions():
                 s.subscribed = True
             else:
                 s.subscribed = False
-            db.session.add(s)
-            db.session.commit()
+                delete_urls_recursively(s.url)
+                db.session.add(s)
+                db.session.commit()
     return redirect(url_for('subscriptions.index'))
 
 def subscribe_to_user(username):
